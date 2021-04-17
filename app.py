5d6f91f24872ee64538dfdb8302d4895ece95397
JamieM-Hub <jamiemckenzie7231@yahoo.co.uk>
2021-04-17 15:28:02 +0000

    add login page with user authentication

--- app.py
+++ app.py
@@ -64,6 +64,32 @@ def delete_pokemon(dex_id):
     return redirect(url_for("get_pokemon"))
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.trainers.find_one(
+            {"username": request.form.get("username")})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password :(")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password :(")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
