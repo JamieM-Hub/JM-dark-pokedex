724a27000104614ef27da1711836120778efb2ca
JamieM-Hub <jamiemckenzie7231@yahoo.co.uk>
2021-04-17 22:51:27 +0000

    add home page

--- app.py
+++ app.py
@@ -21,6 +21,11 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/get_pokemon", methods=["GET", "POST"])
 def get_pokemon():
     pokedex = mongo.db.pokemon.find()
