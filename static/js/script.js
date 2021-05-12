d6878dfb8c32ac10656693f04cb7579d21d7386b
JamieM-Hub <jamiemckenzie7231@yahoo.co.uk>
2021-05-12 11:38:19 +0000

    style type dropdowns to match background-color to type and add text-shadow using CSS and jQuery

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,24 @@
   $(document).ready(function () {
 
+
+      // BACK-END FUNCTIONALITY
       $(".sidenav").sidenav({
           edge: "right"
       });
 
       $("select").formSelect();
       $('.modal').modal();
+
+      // STYLE DROPDOWNS
+      // type dropdown
+      types = ["Normal", "Fire", "Water", "Electric", "Grass", "Ice", "Fighting",
+          "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon"
+      ]
+      types.forEach(type => {
+          $(".select-dropdown li span:contains(" + type + ")").addClass(
+              ["type-" + type.toLowerCase(), "text-shadow"]);
+      });
+
+
+
   });
